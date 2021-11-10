9ca762cf36681030372a0199489a1c0cebbd03d5
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-11-10 15:37:12 -0300

    Requsito 10 a 13

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
+import StrictAccess from './components/StrictAcess';
 class App extends Component {
   render() {
     return (
@@ -12,11 +13,15 @@ class App extends Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/users">Users</Link></li>
+            <li><Link to="/strict-acess">Acesso Restrito</Link></li>
           </ul>
         </nav>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
+        <Switch>
         <Route exact path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" />} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/strict-acess" render={(props) => <StrictAccess {...props} user={{username: 'joao', password: '1234'}} />} />
+        <Route exact path="/" component={Home} />
+        </Switch>
       </BrowserRouter>
     );
   }
