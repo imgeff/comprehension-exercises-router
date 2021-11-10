bc3e08acaedc33c8b927299be7e12abaaae64bbf
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-11-10 14:58:12 -0300

    Requisito 1 a 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/users">Users</Link></li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/users" component={Users} />
+      </BrowserRouter>
     );
   }
 }
